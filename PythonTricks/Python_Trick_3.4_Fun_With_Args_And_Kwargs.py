cdc98cce7a45dac110c658a6466cc1bc20e07e7c
Prashant Patel <itprashantpatel@gmail.com>
2019-02-20 12:49:46 +0530

    Updates

--- PythonTricks/Python_Trick_3.4_Fun_With_Args_And_Kwargs.py
+++ PythonTricks/Python_Trick_3.4_Fun_With_Args_And_Kwargs.py
@@ -11,3 +11,25 @@ def foo(required, *args, **kwargs):
 print(foo('hello'))
 print(foo('hello', 1, 2, 3))
 print(foo('hello', 1, 2, 3, key1='value', key2=999))
+
+# ------------------------------------------------------
+# Forwarding Optional Or Keyword Arguments
+# ------------------------------------------------------
+def foo(x, *args, **kwargs):
+    kwargs['name'] = 'Alice'
+    new_args = args + ('extra', )
+    bar(x, *new_args, **kwargs)
+
+
+class Car:
+    def __init__(self, color, mileage):
+        self.color = color
+        self.mileage = mileage
+
+class AlwaysBlueCar(Car):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.color = 'blue'
+
+print(AlwaysBlueCar('green', 42644).color)
+
