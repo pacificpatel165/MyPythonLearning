d136c5ff675169d02649bb65f23af480e3ef2ae8
Prashant Patel <itprashantpatel@gmail.com>
2019-03-12 09:46:20 +0530

    Updates

---
+++ Python_Cookbook/CookBook_4.13_Creating_Data_Processing_Pipelines.py
@@ -0,0 +1,65 @@
+"""
+Problem
+You want to process data iteratively in the style of a data processing pipeline (similar to
+Unix pipes). For instance, you have a huge amount of data that needs to be processed,
+but it can’t fit entirely into memory.
+"""
+
+# Solution
+# Generator functions are a good way to implement processing pipelines. To illustrate,
+# suppose you have a huge directory of log files that you want to process:
+
+import os
+import fnmatch
+import gzip
+import bz2
+import re
+
+def gen_find(filepat, top):
+    '''
+    Find all filenames in a directory tree that match a shell wildcard pattern
+    '''
+    for path, dirlist, filelist in os.walk(top):
+        for name in fnmatch.filter(filelist, filepat):
+            yield os.path.join(path, name)
+
+
+def gen_opener(filenames):
+    '''
+    Open a sequence of filenames one at a time producing a file object.
+    The file is closed immediately when proceeding to the next iteration.
+    '''
+    for filename in filenames:
+        if filename.endswith('.gz'):
+            f = gzip.open(filename, 'rt')
+        elif filename.endswith('.bz2'):
+            f = bz2.open(filename, 'rt')
+        else:
+            f = open(filename, 'rt')
+        yield f
+        f.close()
+
+
+def gen_concatenate(iterators):
+    '''
+    Chain a sequence of iterators together into a single sequence.
+    '''
+    for it in iterators:
+        yield from it
+
+
+def gen_grep(pattern, lines):
+    '''
+    Look for a regex pattern in a sequence of lines
+    '''
+    pat = re.compile(pattern)
+    for line in lines:
+        if pat.search(line):
+            yield line
+
+lognames = gen_find('access-log*', 'www')
+files = gen_opener(lognames)
+lines = gen_concatenate(files)
+pylines = gen_grep('(?i)python', lines)
+for line in pylines:
+    print(line)
